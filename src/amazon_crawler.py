5c4511674d3b27a115149e37a81a5a805ba1b1c4
tipie101 <timka.timkah@gmail.com>
2020-07-25 10:27:13 +0200

    AmazonLegoSpider: navigate to article view

--- src/amazon_crawler.py
+++ src/amazon_crawler.py
@@ -11,6 +11,7 @@
 # id="price_inside_buybox"
 
 import pickle
+import re
 import scrapy
 from scrapy.crawler import CrawlerProcess
 
@@ -18,10 +19,13 @@ from scrapy.crawler import CrawlerProcess
 amazon_search = []
 lego_articles = [
     {'article_nr': '21129', 'subbrand': 'Minecraft'},
+    {'article_nr': '42111', 'subbrand': 'Technic'},
+    {'article_nr': '21153', 'subbrand': 'Minecraft'},
 ]
-
+art = lego_articles[0]
 
 class AmazonLegoSpider(scrapy.Spider):
+    # a crawler to collect search result links
     name = 'amazon_lego_prices'
     start_urls = [
         '+'.join(['https://www.amazon.de/s?k=lego', art['subbrand'], art['article_nr']]) 
@@ -30,8 +34,17 @@ class AmazonLegoSpider(scrapy.Spider):
 
     def parse(self, response):
         for href in response.css("a::attr(href)").extract():
-            if href.startswith('/LEGO-') and '-21129-' in href:
+            if re.search('^/LEGO(%C2%AE)?-', href) and any('-' + art['article_nr'] + '-' in href for art in lego_articles):
                 print(href)
+                # TODO: Go deeper :D
+                yield scrapy.Request(
+                    response.urljoin(href),
+                    callback=self.parse_sublink
+                )
+
+    def parse_sublink(self, response):
+        print('reached sublink - single article view')
+
 
 if __name__ == '__main__':
     process = CrawlerProcess({
