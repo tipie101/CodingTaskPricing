b9d16c2fea610ee7aa5ef0e02b5f3ddb6b72ab09
tipie101 <timka.timkah@gmail.com>
2020-07-26 21:43:29 +0200

    lint

--- src/amazon_crawler.py
+++ src/amazon_crawler.py
@@ -1,8 +1,6 @@
 # There is an amazon-search API: https://webservices.amazon.com/paapi5/documentation/search-items.html
 # However, to use it one needs the credentials of a fully accepted Amazon Associate
 
-# Ausgangspunkt ist das Suchergebnis von Amazon:
-
 # looking for: <a class="a-size-base a-link-normal a-text-normal" href="/LEGO-...-41343-.../"
 # price is already given via next tag:
 # <span class="a-price" data-a-size="l" data-a-color="base"><span class="a-offscreen">32,09 €</span><span aria-hidden="true"><span class="a-price-whole">32,09</span><span class="a-price-symbol">€</span></span></span>
@@ -35,8 +33,6 @@ class AmazonLegoSpider(scrapy.Spider):
     def parse(self, response):
         for href in response.css("a::attr(href)").extract():
             if re.search('^/LEGO(%C2%AE)?-', href) and any('-' + art['article_nr'] + '-' in href for art in lego_articles):
-                # print(href)
-                # TODO: Go deeper :D
                 yield scrapy.Request(
                     response.urljoin(href),
                     callback=self.parse_sublink
@@ -56,7 +52,6 @@ def crawl():
     process = CrawlerProcess({
     'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
     })
-    # SpiderToysffLego.set_subbrand('city')
     process.crawl(AmazonLegoSpider)
     process.start()
     pickle.dump(results, open( "./data/amazon.p", "wb" ))
